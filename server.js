a0300d94291a999d0330078270cd9b670162bafb
jseb <joel@acc.umu.se>
2010-12-13 23:15:29 +0100

    added /quit command to client, and check if nick name is already in use to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http'),  
-    io = require('socket.io'), // for npm, otherwise use require('./path/to/socket.io') 
+    io = require('/usr/local/lib/node/socket.io'), // for npm, otherwise use require('./path/to/socket.io') 
     fs = require('fs'),
     url = require('url'),
     q = require('querystring')
@@ -38,16 +38,29 @@ server = http.createServer(
                 res.end(); 
                 break;
             case '/join':
-                user = new User(null, url.parse(req.url, true).query.nick);
-                connected_users.push(user);
                 res.writeHead(
                     200,
                     {
                         'Content-Type': 'text/html'
                     }
                 ); 
-                res.write(fs.readFileSync('client.html'));
-                res.end(); 
+                var requestedNick = url.parse(req.url, true).query.nick;
+                var nickTaken = false;
+                for (var i = 0; i < connected_users.length; i++) {
+                    if (connected_users[i].username == requestedNick) {
+                        nickTaken = true;
+                        var response = fs.readFileSync('index.html');
+                        response += "<h2>nick name already in use</h2>";
+                        res.write(response);
+                        res.end();
+                    }
+                }
+                if (!nickTaken) {
+                    user = new User(null, requestedNick);
+                    connected_users.push(user);
+                    res.write(fs.readFileSync('client.html'));
+                    res.end(); 
+                }
                 break;
             default: 
                 res.writeHead(
@@ -74,7 +87,7 @@ function User(client, username) {
 var socket = io.listen(server); 
 socket.on(
     'connection',
-    function(client) { 
+    function(client) {
         connected_users[connected_users.length - 1].client = client;
         var user = connected_users[connected_users.length - 1];
         socket.broadcast("--> " + user.username + " has connected to server.\n");
